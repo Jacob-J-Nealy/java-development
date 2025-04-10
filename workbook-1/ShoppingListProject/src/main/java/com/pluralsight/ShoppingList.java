8345c30689b64aca82ba5c1f646fb6f4e50b01f0
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-04-10 09:27:50 -0400

    Initial Commit

---
+++ workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Jacob's Shopping List");
+        System.out.println("1. Loaves of Bread");
+        System.out.println("2. Bananas");
+        System.out.println("3. Chicken Breast");
+        System.out.println("4. Peanut Butter");
+        System.out.println("5. Grape Jelly");
+        System.out.println("6. Eggs");
+        System.out.println("7. Cheese Slices");
+        System.out.println("8. Turkey Sandwich Slices");
+        System.out.println("9. Butter");
+        System.out.println("10. Brown Rice");
+
+    }
+}

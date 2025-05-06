2ac4e6e766d9c159a18146e2759e6512d1da8a51
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-06 14:41:24 -0400

    Finished Hotel Exercise

--- workbook-4/HotelOperationsModified2/src/main/java/com/pluralsight/Hotel.java
+++ workbook-4/HotelOperationsModified2/src/main/java/com/pluralsight/Hotel.java
@@ -1,4 +1,85 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Hotel {
+
+    // Attributes
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+    // Constructor 1 (int, int)
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfSuites = numberOfSuites;
+        this.bookedSuites = 0;
+        this.bookedBasicRooms = 0;
+    }
+
+
+    // Constructor 2
+    public Hotel(int numberOfRooms, int numberOfSuites,
+                 int bookedSuites, int bookedBasicRooms, String name) {
+        this.name = name;
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfSuites = numberOfSuites;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
+
+    // Derived Getters
+
+    public int getAvailableSuites() {
+        return numberOfSuites - bookedSuites;
+    }
+
+    public int getAvailableRooms() {
+        return numberOfRooms - bookedBasicRooms;
+    }
+
+    public boolean bookRoom() {
+        if (numberOfRooms <= 0) {
+            return false;
+        }
+
+        if (isSuite) {
+            if (getAvailableSuites() >= numberOfRooms) {
+                bookedSuites += numberOfRooms;
+                return true;
+            }
+        } else {
+            if (getAvailableSuites() >= numberOfRooms) {
+                bookedBasicRooms += numberOfRooms;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }

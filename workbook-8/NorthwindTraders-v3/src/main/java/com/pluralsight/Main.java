d5b9cbd0a93381259fec48dd72a85aabc3c66081
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-06-10 12:30:04 -0400

    NorthwindTrader v3

--- workbook-8/NorthwindTraders-v3/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NorthwindTraders-v3/src/main/java/com/pluralsight/Main.java
@@ -1,14 +1,24 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet results = null;
 
+        // Menu
+        System.out.println("What do you want to do?");
+        System.out.println("    1) Display all products");
+        System.out.println("    2) Display all customers");
+        System.out.println("    0) Exit");
+        System.out.print("\nSelect an option: ");
+        int input = scanner.nextInt();
+        scanner.nextLine(); // scanner eater
 
         try {
             if (args.length != 2) {
@@ -22,16 +32,50 @@ public class Main {
             // Connect to Northwind database instead of sakila
             String url = "jdbc:mysql://localhost:3306/Northwind";
 
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
 
-            connection = DriverManager.getConnection(url, username, password);
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
-            String query = """
-                    SELECT ProductID, ProductName, UnitPrice, UnitsInStock
-                    FROM Products
-                    ORDER BY ProductID;
-                    """;
+            // Menu
+            switch (input) {
+                case 1:
+                    displayAllProducts(connection,
+                            preparedStatement,
+                            results);
+                    break;
+                case 2:
+                    displayAllCustomers(connection,
+                            preparedStatement,
+                            results);
+                    break;
+                case 0:
+                    System.out.println("Exiting Program...");
+                    break;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private static void displayAllProducts(Connection connection,
+                                           PreparedStatement preparedStatement,
+                                           ResultSet results) {
+
+        String query = """
+                SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                FROM Products
+                ORDER BY ProductID;
+                """;
+
+        try {
             preparedStatement = connection.prepareStatement(query);
 
             results = preparedStatement.executeQuery();
@@ -83,5 +127,77 @@ public class Main {
                 }
             }
         }
+    }
+
+    private static void displayAllCustomers(Connection connection,
+                                            PreparedStatement preparedStatement,
+                                            ResultSet results) {
+
+        String query = """
+                SELECT CustomerID, ContactName, CompanyName, City, Country, Phone
+                FROM Customers
+                ORDER BY CustomerID;
+                """;
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+
+            results = preparedStatement.executeQuery();
+
+            // Option 1: Stacked Information display
+            while (results.next()) {
+                int CustomerID     = results.getInt(   "CustomerID");
+                String ContactName = results.getString("ContactName");
+                String CompanyName = results.getString("CompanyName");
+                String City        = results.getString("City");
+                String Country     = results.getString("Country");
+                String Phone       = results.getString("Phone");
+
+
+
+                System.out.println("Customer ID: " + CustomerID);
+                System.out.println("Contact Name: " + ContactName);
+                System.out.println("Company Name: " + CompanyName);
+                System.out.println("City: " + City);
+                System.out.println("Country:" + Country);
+                System.out.println("Phone: " + Phone);
+                System.out.println("------------------");
+            }
+
+            results.close();
+            preparedStatement.close();
+            connection.close();
+
+        } catch (Exception ex) {
+            System.out.println("An error has occurred");
+            ex.printStackTrace();
+        } finally {
+
+            if (results != null) {
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+
     }
 }

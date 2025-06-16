3d9cb83943d4835cce441c8efde1c1e587a91f48
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-06-16 11:15:40 -0400

    Exercises

---
+++ workbook-9/NorthwindTradersSpringBoot-v3/src/test/java/com/pluralsight/NorthwindTradersSpringBoot_v3/NorthwindTradersSpringBootV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwindTradersSpringBoot_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersSpringBootV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
